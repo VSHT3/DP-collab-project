a4b6db8a53b990aa0cd4646a7d9f9dcccfa39a65
VSHT3 <alexstefko50@gmail.com>
2026-05-14 11:58:37 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    allowedHosts: ["ab9c-178-40-242-101.ngrok-free.app"],
+  },
+});
